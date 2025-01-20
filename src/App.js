fc2c7242f25edc680033212a8d8408c6c59f5b48
suadcode <suadsalam7@gmail.com>
2025-01-20 20:13:49 +0300

    add to map

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { getPlacesData, getWeatherData } from './api/travelAdvisorAPI';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
+ 
 
 const App = () => {
   const [type, setType] = useState('restaurants');
